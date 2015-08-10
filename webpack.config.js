9a97940dfe9cc56527ff3f1fc4aa97832664569d
Mike Costello <mike@netobjects.com>
2015-08-10 12:02:28 -0400

    use path module

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var path = require("path");
+
 module.exports = {
     entry: "./lib/query-declaration.js",
     output: {
-        path: './dist',
+        path: path.join(__dirname, "dist"),
         filename: "query-declaration.js",
         library: "queryDeclarationAll",
         libraryTarget: "umd"
@@ -11,7 +13,7 @@ module.exports = {
             {
               test: /\.js$/,
               exclude: /(node_modules|bower_components)/,
-              loader: 'babel'
+              loader: "babel"
             }
         ]
     },
